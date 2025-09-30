a36c0a7fd1fa2e9d32bfbec11c6f1e7181ca67be
adaken4 <adakennedy@outlook.com>
2025-09-30 15:33:09 +0300

    feat: add health check handler to return application metrics in JSON format (#3)

--- cmd/cleantown/healthcheck.go
+++ cmd/cleantown/healthcheck.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/adaken4/clean-town/internal/database"
 )
 
 // Declare a handler which writes a plain-text response with information about the
@@ -11,4 +15,17 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	fmt.Fprintln(w, "status: available")
 	fmt.Fprintf(w, "environment: %s\n", app.config.Server.Env)
 	fmt.Fprintf(w, "version: %s\n", version)
+}
+
+func healthHandler(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		metrics := database.CollectMetrics(db)
+
+		w.Header().Set("Content-Type", "application/json")
+		if !metrics.Healthy {
+			w.WriteHeader(http.StatusServiceUnavailable)
+		}
+
+		json.NewEncoder(w).Encode(metrics)
+	}
 }
