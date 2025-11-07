c48c14e1c8e0db15e5c170ba93216a4a2f5c55b5
adaken4 <adakennedy@outlook.com>
2025-11-07 16:42:40 +0300

    feat(middleware): add NewRateLimiter that will return an instance of the rateLimiter (#7)

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -16,3 +16,17 @@ type rateLimiter struct {
 	burst    int
 	logger   *slog.Logger
 }
+
+// NewRateLimiter creates and returns a new rateLimiter instance.
+// Parameters:
+//   - r: rate of requests allowed per second (e.g., 1 means 1 request/sec)
+//   - burst: number of requests allowed to burst beyond the rate
+//   - logger: used for logging warnings and errors
+func NewRateLimiter(r rate.Limit, burst int, logger *slog.Logger) *rateLimiter {
+	return &rateLimiter{
+		limiters: make(map[string]*rate.Limiter),
+		r:        r,
+		burst:    burst,
+		logger:   logger,
+	}
+}
