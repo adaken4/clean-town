98b6841267488d8abcfc14c9a0c3bced4e0e537a
adaken4 <adakennedy@outlook.com>
2025-11-08 13:09:20 +0300

    feat(auth-service): implement SendVerificationEmail function for email verification (#7)

--- internal/services/auth.go
+++ internal/services/auth.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"fmt"
 	"log/slog"
 	"time"
 
@@ -8,6 +9,7 @@ import (
 	"github.com/adaken4/clean-town/internal/config"
 	"github.com/adaken4/clean-town/internal/models"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/wneessen/go-mail"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -42,4 +44,46 @@ func GenerateEmailVerificationToken(signingKey []byte, email string) (string, er
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(signingKey)
+}
+
+// SendVerificationEmail sends an HTML email containing a verification link to the user.
+// It uses Gmail’s SMTP server for delivery.
+func SendVerificationEmail(email, token string, cfg *config.Config) error {
+	m := mail.NewMsg()
+	if err := m.From(cfg.Email.FromAddress); err != nil {
+		return err
+	}
+	if err := m.To(email); err != nil {
+		return err
+	}
+	m.Subject("Email Verification")
+
+	// Construct the verification URL
+	verificationLink := fmt.Sprintf("http://localhost:8080/v1/auth/verify-email?token=%s", token)
+	body := fmt.Sprintf(`
+		<h2>Verify Your Email</h2>
+		<p>Click the link below:</p>
+		<a href="%s">Verify Email</a>
+	`, verificationLink)
+
+	m.SetBodyString(mail.TypeTextHTML, body)
+
+	// Configure SMTP client
+	c, err := mail.NewClient("smtp.gmail.com",
+		mail.WithPort(587),
+		mail.WithSMTPAuth(mail.SMTPAuthPlain),
+		mail.WithUsername(cfg.Email.FromAddress),
+		mail.WithPassword(cfg.Email.AppPassword),
+		mail.WithTLSPolicy(mail.TLSMandatory),
+	)
+	if err != nil {
+		return err
+	}
+
+	// Send the email via SMTP
+	if err := c.DialAndSend(m); err != nil {
+		return err
+	}
+
+	return nil
 }
